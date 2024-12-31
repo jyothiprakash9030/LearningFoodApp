a0c150426e2f77a8a538eaf6fe4c1e725eb9b6d4
Designer <jyothiprakash9076@gmail.com>
2024-12-31 17:09:06 +0530

    fourth commit

--- app/src/main/java/com/example/learningfoodapp/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/learningfoodapp/activities/WelcomeActivity.java
@@ -29,6 +29,6 @@ public class WelcomeActivity extends AppCompatActivity {
     public void register(View view)
     {
 
-        startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
     }
 }
